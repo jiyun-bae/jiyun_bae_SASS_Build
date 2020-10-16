69470457cbeaef55317d75da9a92c0eace906cfa
Jiyun <baejiyun01@gmail.com>
2020-10-16 20:01:40 +0900

    updating changes

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(() => {
+    // make an AJAX request using the Fetch API
+    fetch('./data/classData.json')
+        .then(res => res.json())
+        .then(data => {
+
+            debugger;
+        console.log(data);
+
+        debugger;
+
+        document.getElementById("user").innerHTML = data.coursename;
+        debugger;
+        // handleData
+        // here's where you would call the function that puts the pieces on the page
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+})();
